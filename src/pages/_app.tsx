75d66c60e2a34d3233d554cf5ba51aac45e626ae
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2023-07-21 11:14:57 +0100

    adding a navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
 import "~/styles/globals.css";
+import NavBar from "~/component/NavBar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
+      <NavBar />
       <Component {...pageProps} />
     </ClerkProvider>
   );
